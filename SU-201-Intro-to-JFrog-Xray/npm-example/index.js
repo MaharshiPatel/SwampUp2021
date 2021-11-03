ca0e566ff1041b5f9b69129ea3ea65d4df6494a0
MaharshiPatel <maharshi11patel@gmail.com>
2021-11-03 14:29:47 -0700

    fix dep v3

--- SU-201-Intro-to-JFrog-Xray/npm-example/index.js
+++ SU-201-Intro-to-JFrog-Xray/npm-example/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const _ = require("lodash")
 const mathjs =require("mathjs")
 
